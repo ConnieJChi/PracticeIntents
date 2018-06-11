5c2fe5c741446160c83fad244f34bd2f19dbb8e2
Connie Chi <conniejchi@berkeley.edu>
2018-06-10 20:48:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/practiceintents/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.practiceintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clicked(View v){
+        Intent i = new Intent(Intent.ACTION_SENDTO);
+        i.setData(Uri.parse("mailto:"));
+        i.setType("*/*");
+        i.putExtra(i.EXTRA_SUBJECT, "Happiness!");
+        i.putExtra(i.EXTRA_TEXT, "Happiness is a virtue!");
+        if (i.resolveActivity(getPackageManager()) != null) {
+            startActivity(i);
+        }
+    }
+}
